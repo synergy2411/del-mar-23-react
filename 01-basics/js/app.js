d3584956acf7257c2b0d5a4f37ff8771ef3db4fd
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-06 15:46:05 +0530

    arrow function

--- 01-basics/js/app.js
+++ 01-basics/js/app.js
@@ -87,26 +87,112 @@ console.log(userOne.friends);
 
 // DESTRUCTURING : "unpacking the collection"
 
-let [f1, f2, f3, f4] = userOne.friends;
+// let [f1, f2, f3, f4] = userOne.friends;
 
-f3 = "baz";
+// f3 = "baz";
 
-console.log(userOne.friends[2]);        // baz
+// console.log(userOne.friends[2]);        // baz
 
-let { street: s1, city: c1 } = userOne.address;
+// let { street: s1, city: c1 } = userOne.address;
 
-console.log(s1, c1)
+// console.log(s1, c1)
 
 
-let users = [
-    { email: "foo@test", age: 21 },
-    { email: "bar@test", age: 22 },
-    { email: "bam@test", age: 23 },
-]
+// let users = [
+//     { email: "foo@test", age: 21 },
+//     { email: "bar@test", age: 22 },
+//     { email: "bam@test", age: 23 },
+// ]
 
-let [
-    { email: e1, age: a1 },
-    { email: e2, age: a2 },
-    { email: e3, age: a3 }] = users;
+// let [
+//     { email: e1, age: a1 },
+//     { email: e2, age: a2 },
+//     { email: e3, age: a3 }] = users;
 
-console.log(e1, e2, e3);
+// console.log(e1, e2, e3);
+
+
+
+
+
+let sum = (n1, n2) => {
+    return n1 + n2;
+}
+
+let mul = (n1, n2) => n1 * n2;
+
+let square = val => val * val;
+
+console.log(sum(12, 34))
+
+console.log(mul(2, 3));
+
+console.log(square(4))
+
+// LIMITATION
+// - Does not have 'arguments' keyword
+// - Can't call arrow function with 'new' keyword
+// - Doesn't contain their own 'this' keyword reference
+
+
+// let userTwo = {
+//     firstName: "john",
+//     lastName: "doe",
+//     getFullName: function () {
+//         // this keyword
+//         // return this.firstName + " " + this.lastName;
+//         let that = this;
+//         function dummy() {
+//             // this keyword
+//             return that.firstName + " " + that.lastName;
+//         }
+//         return dummy();
+//     }
+// }
+let userTwo = {
+    firstName: "john",
+    lastName: "doe",
+    getFullName: function () {
+        const dummy = () => this.firstName + " " + this.lastName;
+        return dummy();
+    }
+}
+
+
+console.log(userTwo.getFullName());
+
+
+// Scope Chaining
+
+// let x = 301;
+
+// function a() {
+//     let x = 201;
+
+//     function b() {
+//         console.log(x)
+//     }
+//     b();
+// }
+
+// a();
+
+
+
+
+
+// function Person(firstName, lastName){}
+
+// let john = new Person("foo", "bar")
+
+
+// Object.create()         // instance method
+
+
+// function demo() {
+//     console.log(arguments);
+// }
+
+// demo("test@test")
+// demo("test@test", 32)
+// demo("test@test", 32, true)
