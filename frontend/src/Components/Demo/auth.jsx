363da67ee0ddc9c04f9e0a8fa169ce7f6624a693
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-09 17:05:26 +0530

    validation and error messages

--- frontend/src/Components/Demo/auth.jsx
+++ frontend/src/Components/Demo/auth.jsx
@@ -3,15 +3,24 @@ import React, { Component, createRef } from "react";
 class Auth extends Component {
   usernameInputRef = createRef();
 
-  state = { enteredPassword: "" };
+  state = { enteredPassword: "", error: false };
 
   passwordChangeHandler = (event) =>
     this.setState({ enteredPassword: event.target.value });
 
   submitHandler = (event) => {
     event.preventDefault();
+    if (this.usernameInputRef.current.value.trim() === "") {
+      this.setState({ error: true });
+      return;
+    }
+    if (!this.usernameInputRef.current.value.includes("@")) {
+      this.setState({ error: true });
+      return;
+    }
     console.log("Username :", this.usernameInputRef.current.value);
     console.log("Password :", this.state.enteredPassword);
+    this.setState({ error: false });
   };
 
   render() {
@@ -31,6 +40,12 @@ class Auth extends Component {
                     className="form-control"
                     ref={this.usernameInputRef}
                   />
+
+                  {this.state.error && (
+                    <div className="alert alert-danger">
+                      <p>Something wrong with Username field</p>
+                    </div>
+                  )}
                 </div>
                 {/* password - controlled component */}
                 <label htmlFor="password">Password :</label>
