9605edf01b870c70db6be50670aa4fad587ef91e
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-09 14:59:17 +0530

    form added

--- frontend/src/Components/Expenses/ExpenseForm/ExpenseForm.jsx
+++ frontend/src/Components/Expenses/ExpenseForm/ExpenseForm.jsx
@@ -3,11 +3,16 @@ import React, { Component } from "react";
 class ExpenseForm extends Component {
   state = {
     enteredTitle: "",
+    enteredAmount: "",
+    enteredCreatedAt: "",
   };
 
-  titleChangeHandler = (event) => {
+  titleChangeHandler = (event) =>
     this.setState({ enteredTitle: event.target.value });
-  };
+  amountChangeHandler = (event) =>
+    this.setState({ enteredAmount: event.target.value });
+  createdAtChangeHandler = (event) =>
+    this.setState({ enteredCreatedAt: event.target.value });
 
   render() {
     return (
@@ -17,7 +22,7 @@ class ExpenseForm extends Component {
             <div className="card-body">
               <form>
                 {/* title (Controlled Component)*/}
-                <div className="form-group">
+                <div className="form-group mb-2">
                   <label htmlFor="title">Title :</label>
                   <input
                     type="text"
@@ -28,8 +33,46 @@ class ExpenseForm extends Component {
                   />
                 </div>
                 {/* amount */}
+                <div className="form-group mb-2">
+                  <label htmlFor="amount">Amount :</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    name="amount"
+                    value={this.state.enteredAmount}
+                    onChange={this.amountChangeHandler}
+                    min="0.1"
+                    step="0.1"
+                  />
+                </div>
                 {/* createdAt */}
+                <div className="form-group mb-2">
+                  <label htmlFor="createdAt">Date :</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    name="createdAt"
+                    value={this.state.enteredCreatedAt}
+                    onChange={this.createdAtChangeHandler}
+                    min="2020-04-01"
+                    max="2023-03-31"
+                  />
+                </div>
                 {/* buttons : Add / Cancel*/}
+                <div className="form-group mb-1">
+                  <div className="row">
+                    <div className="col">
+                      <div className="d-grid">
+                        <button className="btn btn-primary">Add</button>
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="d-grid">
+                        <button className="btn btn-light">Cancel</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
