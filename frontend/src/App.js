1782b3b285f1e0bd183f7bd64d3c5d301ac61323
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-13 11:19:34 +0530

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import Expenses from './Components/Expenses/Expenses';
 
 
 import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Auth from './Components/Demo/auth';
 import Parent from './Components/Demo/Parent';
 import AuthContext from './context/auth-context';
 import UseStateDemo from './Components/Demo/UseStateDemo';
 import UseEffectDemo from './Components/Demo/UseEffectDemo';
 import UseContextDemo from './Components/Demo/useContextDemo';
+import Header from './Components/Header/Header';
 
 class App extends Component {
   state = { toggle: true, isLoggedIn: false }
@@ -17,17 +19,15 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='container'>
+        <Header />
         <h1>My Awesome App</h1>
-        {/* <UseEffectDemo /> */}
-
-        {/* <UseStateDemo /> */}
-        {/* <Parent /> */}
-        <AuthContext.Provider value={{ isLoggedIn: this.state.isLoggedIn, setIsLoggedIn: this.changeLogin }}>
-          <UseContextDemo />
-          <Auth />
-          <Expenses />
-        </AuthContext.Provider>
+        <Routes>
+          <Route path='/expenses' element={<Expenses />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/life-cycle' element={<LifeCycle />} />
+          <Route path='/use-effect-demo' element={<UseEffectDemo />} />
+        </Routes>
       </div>
     );
   }
@@ -39,3 +39,10 @@ export default App;
 {/* <button onClick={() => this.setState({ toggle: !this.state.toggle })} className="btn btn-success">
 Show Component</button>
 {this.state.toggle && <LifeCycle />} */}
+
+// eslint-disable-next-line no-lone-blocks
+{/* <AuthContext.Provider value={{ isLoggedIn: this.state.isLoggedIn, setIsLoggedIn: this.changeLogin }}>
+          <UseContextDemo />
+          <Auth />
+          <Expenses />
+        </AuthContext.Provider> */}
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link className="nav-link" to="/auth">
+            Login
+          </Link>
+        </li>
+        <li className="nav-items">
+          <Link className="nav-link" to="/expenses">
+            Expenses
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
