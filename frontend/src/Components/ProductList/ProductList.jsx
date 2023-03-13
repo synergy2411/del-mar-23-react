f6ec65ad6346a3a57ff8b078686cfb1f1cc8cf91
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-13 12:16:48 +0530

    remote server data

--- frontend/src/Components/ProductList/ProductList.jsx
+++ frontend/src/Components/ProductList/ProductList.jsx
@@ -1,10 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductList = () => {
-  useEffect(() => {}, []);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://deloitte-react-mar23-default-rtdb.firebaseio.com/products.json"
+      )
+      .then((resp) => setProducts(resp.data))
+      .catch(console.error);
+  }, []);
   return (
     <div>
       <h4>Product list coming soon...</h4>
+
+      {products.length > 0 && (
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-4" key={product.id}>
+              <div className="card">
+                <div className="card-header">
+                  <h6 className="text-center">
+                    {product.company.toUpperCase()}
+                  </h6>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
